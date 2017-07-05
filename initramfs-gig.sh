60f4c054f7d1af17c97bcfba9cbb2a23d0d94c9b
Maxime Daniel <root@maxux.net>
2017-07-05 09:47:32 +0200

    Grabbing userlist from github organization

--- initramfs-gig.sh
+++ initramfs-gig.sh
@@ -3,7 +3,7 @@
 mkdir -m 600 -p "${ROOTDIR}"/root/.ssh
 rm -f "${ROOTDIR}"/root/.ssh/authorized_keys
 
-for user in $(curl -s https://raw.githubusercontent.com/Jumpscale/developer/master/scripts/devs); do
+for user in $(curl https://api.github.com/orgs/zero-os/members | awk -F'"' '/login/ { print $4 }'); do
     echo "[+] authorizing ssh key: $user"
     curl -s https://github.com/${user}.keys >> "${ROOTDIR}"/root/.ssh/authorized_keys
 done
