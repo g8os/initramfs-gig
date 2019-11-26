bb2830a5838647ddd9808022fc04ce4ec50658cf
Maxime Daniel <root@maxux.net>
2019-11-26 02:16:25 +0100

    Use wget and not curl

--- initramfs-gig.sh
+++ initramfs-gig.sh
@@ -8,7 +8,7 @@ rm -f "${ROOTDIR}"/root/.ssh/authorized_keys
 if [ "${BUILDMODE}" = "debug" ]; then
     for user in $(curl https://api.github.com/orgs/zero-os/members | awk -F'"' '/login/ { print $4 }'); do
         echo "[+] authorizing ssh key: $user"
-        curl -s https://github.com/${user}.keys >> "${ROOTDIR}"/root/.ssh/authorized_keys
+        wget https://github.com/${user}.keys -O- >> "${ROOTDIR}"/root/.ssh/authorized_keys
     done
 
     chmod -f 600 "${ROOTDIR}"/root/.ssh/authorized_keys
