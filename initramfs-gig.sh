e4cbab61afb424eca46625f2dfb6696b9b789cf5
Maxime Daniel <root@maxux.net>
2019-11-26 02:38:34 +0100

    Update curl to wget

--- initramfs-gig.sh
+++ initramfs-gig.sh
@@ -6,7 +6,7 @@ mkdir -m 600 -p "${ROOTDIR}"/root/.ssh
 rm -f "${ROOTDIR}"/root/.ssh/authorized_keys
 
 if [ "${BUILDMODE}" = "debug" ]; then
-    for user in $(curl https://api.github.com/orgs/zero-os/members | awk -F'"' '/login/ { print $4 }'); do
+    for user in $(wget https://api.github.com/orgs/zero-os/members -O- | awk -F'"' '/login/ { print $4 }'); do
         echo "[+] authorizing ssh key: $user"
         wget https://github.com/${user}.keys -O- >> "${ROOTDIR}"/root/.ssh/authorized_keys
     done
